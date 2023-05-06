68ba95735d67d76d0c76483fbc4119a1fbcca3aa
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-05-06 07:39:55 +0100

    DataStructure (List)

---
+++ dataStructures/DataStructure.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  /*List*/
+  List myList = [1, 2, 3, 4, 5]; //dynamic list
+  print(myList.length);
+  print(myList[0]);
+  print(myList.reversed);
+  List<double> anotherList = [
+    1.0,
+    2.0,
+    3.0,
+    4.0,
+    5.0
+  ]; //list of double elements
+
+  final justAnotherList = [2, 4, 6, 8, 0];
+
+  <int>[11,22,33];
+}
